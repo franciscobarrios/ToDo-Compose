af26b3ed6402e89c3a82a83c06d6c1c0357ed770
Francisco Barrios <francisco.barrios@ifyoucan.com>
2020-10-22 07:51:35 +0700

    creating a list

--- app/src/main/java/com/fjbg/todo/activity/MainActivity.kt
+++ app/src/main/java/com/fjbg/todo/activity/MainActivity.kt
@@ -3,11 +3,13 @@ package com.fjbg.todo.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.lazy.LazyColumnForIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import com.fjbg.todo.R
+import com.fjbg.todo.data.Task
 import com.fjbg.todo.ui.Greeting
 import com.fjbg.todo.ui.ToDoTheme
 
@@ -19,8 +21,41 @@ class MainActivity : AppCompatActivity() {
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting(resources.getString(R.string.app_name))
                 }
+                LazyColumnIndexed(fakeData())
             }
         }
+
+        //val taskId: Int,
+        //    val taskTitle: String,
+        //    val taskContent: String?,
+        //    val taskCompleted: Boolean
+
+
     }
 }
 
+fun fakeData(): ArrayList<Task> {
+    val task = Task(
+        taskId = 0,
+        taskTitle = "this is a task",
+        taskContent = "this is the content of the task",
+        taskCompleted = false
+    )
+
+    val list = ArrayList<Task>()
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+
+    return list
+}
+
+@Composable
+fun LazyColumnIndexed(tasks: List<Task>) {
+    LazyColumnForIndexed(items = tasks) { index, item ->
+        Text(text = item.taskTitle)
+        item.taskContent?.let { Text(text = it) }
+    }
+}
