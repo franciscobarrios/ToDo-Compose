c4b437aebac50575306600ec6790dd0c1b05f638
Francisco Barrios <francisco.barrios@ifyoucan.com>
2020-10-24 09:26:53 +0700

    adding style to the tasks

--- app/src/main/java/com/fjbg/todo/activity/MainActivity.kt
+++ app/src/main/java/com/fjbg/todo/activity/MainActivity.kt
@@ -3,11 +3,18 @@ package com.fjbg.todo.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.lazy.LazyColumnForIndexed
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fjbg.todo.R
 import com.fjbg.todo.data.Task
 import com.fjbg.todo.ui.Greeting
@@ -24,20 +31,13 @@ class MainActivity : AppCompatActivity() {
                 LazyColumnIndexed(fakeData())
             }
         }
-
-        //val taskId: Int,
-        //    val taskTitle: String,
-        //    val taskContent: String?,
-        //    val taskCompleted: Boolean
-
-
     }
 }
 
 fun fakeData(): ArrayList<Task> {
     val task = Task(
         taskId = 0,
-        taskTitle = "this is a task",
+        taskTitle = "This is a task",
         taskContent = "this is the content of the task",
         taskCompleted = false
     )
@@ -48,14 +48,74 @@ fun fakeData(): ArrayList<Task> {
     list.add(task)
     list.add(task)
     list.add(task)
-
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
+    list.add(task)
     return list
+}
+
+fun mod(): Modifier {
+    return Modifier.padding(
+        start = 12.dp,
+        top = 36.dp,
+        end = 12.dp,
+        bottom = 12.dp
+    )
 }
 
 @Composable
 fun LazyColumnIndexed(tasks: List<Task>) {
-    LazyColumnForIndexed(items = tasks) { index, item ->
-        Text(text = item.taskTitle)
-        item.taskContent?.let { Text(text = it) }
+    LazyColumnFor(
+        items = tasks,
+        modifier = mod()
+    ) { item ->
+        Text(
+            text = item.taskTitle,
+            style = TextStyle(
+                fontFamily = FontFamily.Monospace,
+                fontSize = 18.sp,
+                color = Color.DarkGray
+            )
+        )
+        item.taskContent?.let {
+            Text(
+                text = it,
+                style = TextStyle(
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 12.sp,
+                    color = Color.Gray
+                )
+            )
+        }
+        Text(
+            text = taskStatus(item.taskCompleted),
+            style = TextStyle(
+                fontFamily = FontFamily.Monospace,
+                fontSize = 12.sp,
+                color = Color.Gray
+            )
+        )
+    }
+}
+
+fun taskStatus(status: Boolean): String {
+    return if (status) {
+        "Completed"
+    } else {
+        "Uncompleted"
     }
 }
