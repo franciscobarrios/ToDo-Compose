76c4d5188d60bcd93319d517ee3eecbe542a5458
Francisco Barrios <francisco.barrios@ifyoucan.com>
2020-11-24 08:47:21 +0700

    small fixes, description added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ToDo-Compose
 Yet Another To Do app, this time with Jetpack Compose
+
+* Jetpack Compose
+* Room
+* MVVM
+* Hilt
+* Coroutine 
--- app/src/main/java/com/fjbg/todo/ui/detail/DetailTaskActivity.kt
+++ app/src/main/java/com/fjbg/todo/ui/detail/DetailTaskActivity.kt
@@ -1,12 +1,10 @@
 package com.fjbg.todo.ui.detail
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.setContent
-import com.fjbg.todo.ui.TAG
 import com.fjbg.todo.ui.main.task_id
 import com.fjbg.todo.ui.theme.ToDoTheme
 import dagger.hilt.android.AndroidEntryPoint
